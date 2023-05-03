befa8a5ef536da3b480573713994c4bcf5ec05be
Pallavi Patil <pallavishravan545@gmail.com>
2023-05-03 18:56:03 +0530

    New Screen added

---
+++ lib/Home_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Home_Screen extends StatefulWidget {
+  const Home_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Home_Screen> createState() => _Home_ScreenState();
+}
+
+class _Home_ScreenState extends State<Home_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Screen'),),
+    );
+  }
+}
